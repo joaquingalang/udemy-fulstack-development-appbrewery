74438d46fc80c0a648568e40cd78c6b5f79e1af2
joaquingalang <galangjoaquin@gmail.com>
2025-05-07 11:31:22 +0800

    Add: 16.2 Document Object Model

---
+++ 16.2 Document Object Model/index.js
@@ -0,0 +1,8 @@
+var targetElement = document.firstElementChild.querySelector("body").querySelector("ul").lastElementChild;
+targetElement.innerHTML = "Joaquin";
+
+var targetLink = document.querySelector(".list a");
+targetLink.style.color = "red";
+
+var targetButton = document.querySelector("button");
+targetButton.style.backgroundColor = "yellow";
